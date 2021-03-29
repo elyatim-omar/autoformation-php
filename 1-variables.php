1a308ad81257820812cf39b9613092347eab56b9
elyatim-omar <elyatim.omar.dev@gmail.com>
2021-03-29 09:29:15 +0100

    autoformation

---
+++ 1-variables.php
@@ -0,0 +1,51 @@
+<?php 
+// $age = 24 ;
+// $nom = "omar";
+// echo $age,' ' ,$nom;
+
+// type float 
+// $poid = 57.3;
+// echo $poid;
+
+// type bool
+// $true = true;
+// $false = false;
+// echo $true ;
+// echo "<br>";
+// echo $false ;
+
+// type null 
+// $pas_de_valeurs = null;
+// echo $pas_de_valeurs;
+
+// la concatenation avec des guillemets double
+// $age = 24;
+// echo "omar a $age ans"
+
+// la concatenation avec des guillemets simple
+// $age = 24;
+// echo 'omar a '. $age .' ans';
+
+// des calculs simples
+$num = 2 + 4 ;
+$num1 = 5 - 1 ;
+$num2 = 3 * 3 ;
+$num3 = 10 / 10 ;
+$num4 = 3 * 5 + 1 ; 
+$num5 = ($num3 + 1) * $num ;
+echo $num ;
+echo "<br>";
+echo $num1 ;
+echo "<br>";
+echo $num2 ;
+echo "<br>";
+echo $num3 ;
+echo "<br>";
+echo $num4 ;
+echo "<br>";
+echo $num5 ;
+echo "<br>";
+
+
+
+?>
